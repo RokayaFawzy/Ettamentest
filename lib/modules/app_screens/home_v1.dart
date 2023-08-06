55d197799b6f3476b8a33a8a2b56ee5b711673e4
YAS <yaserabusamra@gmail.com>
2023-08-06 14:55:20 +0300

    fix home 1 ui

--- lib/modules/app_screens/home_v1.dart
+++ lib/modules/app_screens/home_v1.dart
@@ -12,6 +12,167 @@ class HomeV1 extends StatefulWidget {
 class _HomeV1State extends State<HomeV1> {
   @override
   Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+        body: SafeArea(
+          child: ListView(children: [
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0, left: 35, bottom: 20),
+                  child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'My Appointments',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.indigo[600],
+                        ),
+                      )),
+                ),
+                Row(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      height: MediaQuery.of(context).size.width * 0.5,
+                      padding: const EdgeInsets.all(10),
+                      child: Container(
+                          color: Colors.blue[900],
+                          child: const Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                'January',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20),
+                              ),
+                              Text(
+                                '21',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 50),
+                              ),
+                              Text(
+                                'at 4:30pm',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20),
+                              )
+                            ],
+                          )),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      padding: const EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              SizedBox(
+                                  height: 70,
+                                  width: 70,
+                                  child: Image.asset(
+                                    'assets/images/123.jpg',
+                                    fit: BoxFit.cover,
+                                  )),
+                              const SizedBox(width: 5,),
+                              Expanded(
+                                child: SizedBox(
+                                  height: 60,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Row(
+                                            children: [
+                                              Icon(
+                                                Icons.star,
+                                                color: Colors.amber[800],
+                                                size: 20,
+                                              ),
+                                              const SizedBox(
+                                                width: 5,
+                                              ),
+                                              Text(
+                                                '5.0',
+                                                style: TextStyle(
+                                                    color:
+                                                    Colors.grey[700]),
+                                              ),
+                                            ],
+                                          ),
+                                          Icon(
+                                            Icons.more_vert,
+                                            color: Colors.grey[600],
+                                          )
+                                        ],
+                                      ),
+                                      const Text('\$24')
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 15),
+                            width: double.infinity,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  width: double.infinity,
+                                  child: Text(
+                                    'Dr.Georgia Griffin',
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(
+                                        color: Colors.indigo[900],
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 17),
+                                  ),
+                                ),
+                                const SizedBox(height: 5,),
+                                SizedBox(
+                                  width: double.infinity,
+                                  child: Text(
+                                    'Cardiologist',
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(color: Colors.grey[600]),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            width: double.infinity,
+                            height: 40,
+                            child: ElevatedButton(
+                              onPressed: () { },
+                              style: ElevatedButton.styleFrom(
+                                primary: const Color.fromARGB(255, 8, 229, 89),
+                                elevation: 0,
+                              ),
+                              child: const Text('Send Message'),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ])
+      )
+    );
+
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
@@ -29,8 +190,7 @@ class _HomeV1State extends State<HomeV1> {
                       Column(
                         children: [
                           Padding(
-                            padding: const EdgeInsets.only(
-                                top: 20.0, left: 35, bottom: 20),
+                            padding: const EdgeInsets.only(top: 20.0, left: 35, bottom: 20),
                             child: Align(
                                 alignment: Alignment.centerLeft,
                                 child: Text(
@@ -47,8 +207,8 @@ class _HomeV1State extends State<HomeV1> {
                               Container(
                                   color: Colors.blue[900],
                                   height: 220,
-                                  child: Column(
-                                    children: const [
+                                  child: const Column(
+                                    children: [
                                       Padding(
                                         padding: EdgeInsets.only(top: 20.0),
                                         child: Text(
@@ -78,95 +238,97 @@ class _HomeV1State extends State<HomeV1> {
                                       )
                                     ],
                                   )),
-                              Column(
-                                children: [
-                                  Row(
-                                    children: [
-                                      Padding(
-                                        padding:
-                                            const EdgeInsets.only(bottom: 20.0),
-                                        child: SizedBox(
-                                            height: 90,
-                                            width: 100,
-                                            child: Image.asset(
-                                              'assets/images/123.jpg',
-                                            )),
-                                      ),
-                                      Column(
-                                        children: [
-                                          Padding(
-                                            padding: const EdgeInsets.only(
-                                                bottom: 40.0),
-                                            child: Align(
-                                                alignment: Alignment.topCenter,
-                                                child: Row(
-                                                  children: [
-                                                    Icon(
-                                                      Icons.star,
-                                                      color: Colors.amber[800],
-                                                    ),
-                                                    Text(
-                                                      '5.0',
-                                                      style: TextStyle(
-                                                          color:
-                                                              Colors.grey[700]),
-                                                    ),
-                                                    Padding(
-                                                      padding: const EdgeInsets
-                                                          .only(),
-                                                      child: Icon(
-                                                        Icons.more_vert,
-                                                        color: Colors.grey[600],
-                                                      ),
-                                                    )
-                                                  ],
-                                                )),
-                                          ),
-                                          Padding(
-                                            padding: const EdgeInsets.only(
-                                              right: 30,
-                                            ),
-                                            child: Text('\$24'),
-                                          )
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                  Text(
-                                    'Dr.Georgia Griffin',
-                                    style: TextStyle(
-                                        color: Colors.indigo[900],
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 17),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        top: 8.0, right: 50),
-                                    child: Text(
-                                      'Cardiologist',
-                                      style: TextStyle(color: Colors.grey[600]),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        top: 20.0, left: 30),
-                                    child: SizedBox(
-                                      width: 200,
-                                      height: 40,
-                                      child: ElevatedButton(
-                                        onPressed: () {
-                                          // Handle button tap here
-                                        },
-                                        style: ElevatedButton.styleFrom(
-                                          primary:
-                                              Color.fromARGB(255, 8, 229, 89),
-                                          elevation: 5,
+                              Container(
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Padding(
+                                          padding:
+                                              const EdgeInsets.only(bottom: 20.0),
+                                          child: SizedBox(
+                                              height: 90,
+                                              width: 100,
+                                              child: Image.asset(
+                                                'assets/images/123.jpg',
+                                              )),
                                         ),
-                                        child: Text('Send Message'),
+                                        Column(
+                                          children: [
+                                            Padding(
+                                              padding: const EdgeInsets.only(
+                                                  bottom: 40.0),
+                                              child: Align(
+                                                  alignment: Alignment.topCenter,
+                                                  child: Row(
+                                                    children: [
+                                                      Icon(
+                                                        Icons.star,
+                                                        color: Colors.amber[800],
+                                                      ),
+                                                      Text(
+                                                        '5.0',
+                                                        style: TextStyle(
+                                                            color:
+                                                                Colors.grey[700]),
+                                                      ),
+                                                      Padding(
+                                                        padding: const EdgeInsets
+                                                            .only(),
+                                                        child: Icon(
+                                                          Icons.more_vert,
+                                                          color: Colors.grey[600],
+                                                        ),
+                                                      )
+                                                    ],
+                                                  )),
+                                            ),
+                                            const Padding(
+                                              padding: EdgeInsets.only(
+                                                right: 30,
+                                              ),
+                                              child: Text('\$24'),
+                                            )
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                    Text(
+                                      'Dr.Georgia Griffin',
+                                      style: TextStyle(
+                                          color: Colors.indigo[900],
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 17),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          top: 8.0, right: 50),
+                                      child: Text(
+                                        'Cardiologist',
+                                        style: TextStyle(color: Colors.grey[600]),
                                       ),
                                     ),
-                                  )
-                                ],
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          top: 20.0, left: 30),
+                                      child: SizedBox(
+                                        width: 200,
+                                        height: 40,
+                                        child: ElevatedButton(
+                                          onPressed: () {
+                                            // Handle button tap here
+                                          },
+                                          style: ElevatedButton.styleFrom(
+                                            primary:
+                                                const Color.fromARGB(255, 8, 229, 89),
+                                            elevation: 5,
+                                          ),
+                                          child: const Text('Send Message'),
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
                               )
                             ],
                           ),
@@ -232,7 +394,7 @@ class _HomeV1State extends State<HomeV1> {
                 ),
               ),
               const SizedBox(height: 20),
-              Divider(
+              const Divider(
                 color: kPrimaryColor,
                 thickness: 0.1,
                 height: 8,
